503ceb3a0cd80a22b4b7616b8ab86242749595c6
Daniel B Kang <danielbkang91@gmail.com>
2024-02-11 03:05:04 -0800

    Change title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'yes-we-can-driving-school-angular';
+  title = 'Yes We Can Driving School';
 }
