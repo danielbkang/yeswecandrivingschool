4bcaf7088b6f94bf650a2117b0b4ee2c065f3d8d
Daniel B Kang <danielbkang91@gmail.com>
2024-02-11 21:33:06 -0800

    Add available service region

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, MatListModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css',
 })
